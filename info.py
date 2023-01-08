d72e132f39030d930b498dd1b880bb9f95451cfe
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-08 21:39:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,9 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001858473447))
 RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -1001858473447)
 
 # Bot Options
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
+IMDB = is_enabled((environ.get('IMDB', "False")), False)
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
