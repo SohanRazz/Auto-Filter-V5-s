9de2f82f4fe2db34f89064171adc8d59f4b66d8e
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-02-27 23:45:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -100918566682))
-RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -10091856668))
+RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', -100918566682))
 
 # Bot Options
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
